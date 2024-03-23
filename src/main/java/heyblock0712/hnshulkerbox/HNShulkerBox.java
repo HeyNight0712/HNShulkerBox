00869751e985f9e4885197b7ad378fd5947e40c7
blacknight0712 <blacknight0981@gmail.com>
2024-03-24 06:00:48 +0800

    Initial commit

---
+++ src/main/java/heyblock0712/hnshulkerbox/HNShulkerBox.java
@@ -0,0 +1,17 @@
+package heyblock0712.hnshulkerbox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HNShulkerBox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
