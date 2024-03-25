e4715d526f2cf4e2d7cbc4d3d1d1eb24d21041bf
blacknight0712 <blacknight0981@gmail.com>
2024-03-25 16:30:54 +0800

    修復 盒子疊代

--- src/main/java/heyblock0712/hnshulkerbox/listeners/InventoryClick.java
+++ src/main/java/heyblock0712/hnshulkerbox/listeners/InventoryClick.java
@@ -1,6 +1,7 @@
 package heyblock0712.hnshulkerbox.listeners;
 
 import heyblock0712.hnshulkerbox.data.Backpack;
+import heyblock0712.hnshulkerbox.utils.ShulkerBoxUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,34 +13,32 @@ public class InventoryClick implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
         // 獲取 移動物品
-        ItemStack itemClicked = event.getCurrentItem();
-
-
         Player player = (Player) event.getWhoClicked();
-        ItemStack originalItem = Backpack.peekItem(player);
 
-        // 獲取 主手槽位
+        // 過濾
+        if (!Backpack.hasPlayer(player)) return;
+
+        ItemStack itemClicked = event.getCurrentItem();
+        ItemStack originalItem = Backpack.peekItem(player);
         int mainHandSlot = player.getInventory().getHeldItemSlot();
 
-        if (event.getHotbarButton() == mainHandSlot) {
+        // 儲存新位置
+        player.sendMessage(String.valueOf(event.getSlot()));
+        ItemStack hotbarButton = null;
+
+        if (event.getHotbarButton() != -1) {
+            hotbarButton = player.getInventory().getItem(event.getHotbarButton());
+        }
+        // 是否 移動原先項目
+
+        // 檢查 快捷鍵移動
+        if (event.getHotbarButton() == mainHandSlot || hotbarButton != null && ShulkerBoxUtil.isShulkerBox(hotbarButton.getType())) {
             event.setCancelled(true);
             return;
         }
 
-        if (originalItem != null && originalItem.equals(itemClicked)) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    public void onInventoryDrag(InventoryDragEvent event) {
-        // 獲取移動物品
-        ItemStack itemClicked = event.getOldCursor();
-
-        Player player = (Player) event.getWhoClicked();
-        ItemStack originalItem = Backpack.peekItem(player);
-
-        if (originalItem != null && originalItem.equals(itemClicked)) {
+        // 檢查 點選移動
+        if (originalItem != null && originalItem.equals(itemClicked) || itemClicked != null && ShulkerBoxUtil.isShulkerBox(itemClicked.getType())) {
             event.setCancelled(true);
         }
     }
