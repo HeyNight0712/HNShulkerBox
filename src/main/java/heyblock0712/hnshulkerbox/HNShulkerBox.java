aec87d3460c1a023f983ddf4ad60e408e99a6b4b
blacknight0712 <blacknight0981@gmail.com>
2024-03-27 03:09:22 +0800

    debug 通知關閉

--- src/main/java/heyblock0712/hnshulkerbox/HNShulkerBox.java
+++ src/main/java/heyblock0712/hnshulkerbox/HNShulkerBox.java
@@ -11,7 +11,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HNShulkerBox extends JavaPlugin {
     // Debug 用
-    public static boolean debug = true;
+    public static boolean debug = false;
 
     @Override
     public void onEnable() {
